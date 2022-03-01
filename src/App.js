141c66d8c978bdbde74c8e8b94a5f829db6a83a7
Ali Gh <aligodosi@gmail.com>
2022-03-01 19:32:05 +0330

    init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from "react-query"
+import { ReactQueryDevtools } from "react-query/devtools"
+
+import { InfinitePeople } from "./components/people/InfinitePeople"
+
+const queryClient = new QueryClient()
+
+function App() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+                <h1>Infinite People</h1>
+                <InfinitePeople />
+
+                <ReactQueryDevtools />
+            </div>
+        </QueryClientProvider>
+    );
+}
+
+export default App;
